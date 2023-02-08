6997daa98aebeb38618a24b72fc5533370e1aa85
Janu1523 <124561407+Janu1523@users.noreply.github.com>
2023-02-08 10:29:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
       
- expect URLError as e:
+expect URLError as e:
   streamlit.error()
     
 #import requests
